085515c6cfa964d86696b913a5f142c5fa7b5822
Andrei Listopadov <andrew.listopadov@gmail.com>
2019-08-07 13:51:46 +0300

    updates basic example;

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { fromEvent, pipe } from 'rxjs';
-import { throttleTime } from 'rxjs/operators';
 import './styles/index.css';
+import { fromEvent, pipe } from 'rxjs';
+import { throttleTime, map } from 'rxjs/operators';
 
 const button = document.querySelector('button');
 fromEvent(button, 'click')
-  .pipe(throttleTime(1000))
-  .subscribe(event => {
-    return console.log(event);
+  .pipe(
+    throttleTime(1000),
+    map((event: MouseEvent) => `x: ${event.clientX}; y: ${event.clientY};`)
+  )
+  .subscribe(coordinates => {
+    return console.log(coordinates);
   });
